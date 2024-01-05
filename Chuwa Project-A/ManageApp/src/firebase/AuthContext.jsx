f2cff83f5f8d6c016b85847e0a6f5b8d3444d9d0
Weslie2023 <swen3@stevens.edu>
2024-01-05 14:03:59 -0800

    1. Navigate to login page only if user wants to access products without login.

--- Chuwa Project-A/ManageApp/src/firebase/AuthContext.jsx
+++ Chuwa Project-A/ManageApp/src/firebase/AuthContext.jsx
@@ -1,17 +1,20 @@
 import React, {createContext, useState, useEffect, useContext} from 'react';
-import {useNavigate} from "react-router-dom";
+import {useNavigate, useLocation} from "react-router-dom";
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate(); // 使用useNavigate钩子
-
+    const location = useLocation();
     useEffect(() => {
         fetch('http://localhost:3000/auth', {credentials: 'include'})
             .then(response => {
                 if (response.status === 401) {
-                    navigate('/login'); // 使用navigate进行导航
+                    if (location.pathname === '/products') {
+                        // 只有当用户尝试访问 /products 而未登录时才重定向到 Login
+                        navigate('/login');
+                    }
                     return;
                 }
                 return response.json()
@@ -19,7 +22,7 @@ export const AuthProvider = ({children}) => {
             .then(data => {
                 setUser(data.user);
             });
-    }, []);
+    }, [location.pathname]);
 
     return (
         <AuthContext.Provider value={{user, setUser}}>

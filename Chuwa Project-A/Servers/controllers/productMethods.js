23f513961f3a6efc091947292e3cd16c9d8625b0
Guangwei Na <gna@stevens.edu>
2024-01-07 13:25:35 -0500

    Add  getProductsByUserId route

--- Chuwa Project-A/Servers/controllers/productMethods.js
+++ Chuwa Project-A/Servers/controllers/productMethods.js
@@ -1,4 +1,5 @@
 const Product = require('../models/Product');
+const User = require('../models/User');
 
 const creatProduct = async (req, res) => {
     try{
@@ -16,6 +17,34 @@ const getAllProducts = async (req, res) => {
     try{
         const products = await Product.find();
         res.json(products);
+    }catch (err){
+        console.error(err.message);
+        res.status(500).json({message: 'Server Error'});
+    }
+}
+
+const getProductsByUserId = async (req, res) => {
+    let cart = null;
+    try {
+        const userId = req.params.id;
+        const user = await User.findById(userId);
+        cart = user.cart;
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: 'Server Error' });
+    }
+    try{
+        const products = await Product.find();
+        //需要Cart里加一个product id的字段
+        const result = products.map((product) => {
+            const cartItem = cart.find((item) => item.id === product.id);
+            const selectedQuantity = cartItem? cartItem.quantity: 0;
+            return {
+                ...product,
+                selected:selectedQuantity
+            };
+        })
+        res.json(result);
     }catch (err){
         console.error(err.message);
         res.status(500).json({message: 'Server Error'});
